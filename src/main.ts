fe2a0d1c788626428cdb100e2f731b0b3c661faa
likuny0415 <li.kuny@husky.neu.edu>
2022-04-04 21:38:30 +0800

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.setGlobalPrefix("api/v1")
+  app.enableCors()
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
